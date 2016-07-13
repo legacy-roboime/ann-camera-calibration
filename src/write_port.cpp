92bb5eff7241e1c03cd002adb3584142e7481691
Gustavo Claudio Karl Couto <gustavokcouto@uol.com.br>
2016-07-12 21:33:40 -0300

    Testing omni equantions in robo

--- src/write_port.cpp
+++ src/write_port.cpp
@@ -33,33 +33,26 @@ void init_serial() {
 // This example reads the contents of a file and writes the entire
 // file to the serial port one character at a time.
 int main(int argc, char **argv) {
-  if(argc < 3) {
-    printf(" Must provide at least two arg\n");
-    exit(1);
-  }
-
   init_serial();
 
-  // Do not skip whitespace characters while reading from the
-  // serial port.
-  //
-  // serial_port.unsetf( std::ios_base::skipws ) ;
-  //
   char package[5];
   package[0] = 'a';
-  int delay   = atoi(argv[1]),
-      num_pkg = atoi(argv[2]), n = 0;
-  int vel[3];
-  int num = 0;
-  while (n++ < num_pkg) {
-    std::cin >> package[1] >> package[2] >> package[3];
+  package[4] = 0;
+  int v[3];
+ 
+  while (true) {
+    std::cin >> v[1] >> v[2] >> v[3];
     //input_file.read(&next_byte, 1);
+    for(int i = 0; i< 3; i++){
+      package[i+1] = v[i];
+    }
     for(int i = 0; i < 5; i++){
       serial_port.write(&package[i], 1);
+      std::cerr << package[i] << " ";
     }
+    std::cerr << "\n";
     // Print a '.' for every character read from the input file.
-    //std::cerr <<  << " " << num++ << std::endl;
-    usleep(delay);
+    usleep(100000);
   }
 
   return EXIT_SUCCESS;
